f85b3f5c342edadd3099e33a1c905ba9bf53e510
Indeeana_Swede <whyshoulditellyouifidontknow@gmail.com>
2024-05-23 15:06:54 +0200

    added mounting component and repeating components

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 import '../css/styling.scss';
 import * as bootstrap from "bootstrap";
 
-const grab = el => document.querySelector(el);
+const grab = elem => document.querySelector(elem);
 
 const filefetcher = async theurl => (await (await (fetch(theurl))).text()).replace(/<script.+?vite\/client.+?<\/script>/g, '');
 
@@ -12,4 +12,26 @@ function newElement(element, html, remove = true) {
     element.after(elemond, element);
   }
   remove && element.remove();
+}
+
+async function setUpComponent() {
+  while (true) {
+    let comp = grab('component');
+    if (!comp) { break; }
+    let source = `/html${comp.getAttribute('src')}.html`;
+    let html = await filefetcher(source);
+    newElement(comp, html)
+  }
+  repeatingElements();
+}
+
+function repeatingElements() {
+  while (true) {
+    let repeater = grab('[repeat]');
+    if (!r) { break; }
+    let repCount = Math.max(1, +r.getAttribute('repeat'));
+    for (let i = 0; i < repCount - 1, i++;) {
+      newElement(repeater, html, false);
+    }
+  }
 }
