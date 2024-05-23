04a708acb8806466e240408afba502fdec02c970
Indeeana_Swede <whyshoulditellyouifidontknow@gmail.com>
2024-05-23 14:48:11 +0200

    made some changes to nav's addresses and included bootstrap to package dependencies as well as added some javaScript to create and replace elements

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,15 @@
 import '../css/styling.scss';
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
+
+const grab = el => document.querySelector(el);
+
+const filefetcher = async theurl => (await (await (fetch(theurl))).text()).replace(/<script.+?vite\/client.+?<\/script>/g, '');
+
+function newElement(element, html, remove = true) {
+  let newEl = document.createElement('div');
+  newEl.innerHTML = html;
+  for (let elemond of [...newEl.children]) {
+    element.after(elemond, element);
+  }
+  remove && element.remove();
+}
